f3071888523234eea8a139202e75636526dba3a9
Makram ELGHAZZAOUI <makram.elghazzaoui@arteris.com>
2022-01-18 09:50:20 +0100

    move utils callses to utils folder

--- FolderComparer.cs
+++ FolderComparer.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
 using System.Text.RegularExpressions;
+using ImgComparer.Utils;
 
 namespace ImgComparer
 {
--- Utils/ImageUtil.cs
+++ Utils/ImageUtil.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ImgComparer
+namespace ImgComparer.Utils
 {
     class ImageUtil
     {
